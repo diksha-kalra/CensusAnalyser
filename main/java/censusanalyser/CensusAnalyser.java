235a482fc8e2d72894294598a98941656b1369be
diksha-kalra <kalradiksha11@gmail.com>
2020-10-20 00:01:25 +0530

    Added: when file name correct but file type is incorrect throw custom exception

--- main/java/censusanalyser/CensusAnalyser.java
+++ main/java/censusanalyser/CensusAnalyser.java
@@ -7,27 +7,27 @@ import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Iterator;
+import java.util.stream.StreamSupport;
 
 public class CensusAnalyser {
 
 	public int loadIndiaCensusData(String csvFilePath) throws CensusAnalyserException {
-		try {
-			Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));
+		try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));){
 			CsvToBeanBuilder<IndiaCensusCSV> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
 			csvToBeanBuilder.withType(IndiaCensusCSV.class);
 			csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
 			CsvToBean<IndiaCensusCSV> csvToBean = csvToBeanBuilder.build();
 			Iterator<IndiaCensusCSV> censusCSVIterator = csvToBean.iterator();
-			;
 			int numOfEnteries = 0;
-			while (censusCSVIterator.hasNext()) {
-				numOfEnteries++;
-				IndiaCensusCSV censusData = censusCSVIterator.next();
-			}
+			Iterable<IndiaCensusCSV> csvIterable=()->censusCSVIterator;
+			numOfEnteries=(int)StreamSupport.stream(csvIterable.spliterator(),false).count();
 			return numOfEnteries;
 		} catch (IOException e) {
 			throw new CensusAnalyserException(e.getMessage(),
 					CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
+		}catch(IllegalStateException e) {
+			throw new CensusAnalyserException(e.getMessage(),
+					CensusAnalyserException.ExceptionType.UNABLE_TO_PARSE);
 		}
 	}
 }
